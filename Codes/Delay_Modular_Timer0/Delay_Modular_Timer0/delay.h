52d7f460bd52bc53378251473216a0fe74e78a3f
Sreejith Rajan <sreejithkriisc@gmail.com>
2025-09-20 00:26:41 +0530

    added an example for modular style programming to create delay_ms using timer in CTC mode

---
+++ Codes/Delay_Modular_Timer0/Delay_Modular_Timer0/delay.h
@@ -0,0 +1,16 @@
+/*
+ * delay.h
+ *
+ * Created: 19-09-2025 23:48:37
+ *  Author: sreej
+ */ 
+
+
+#ifndef DELAY_H_
+#define DELAY_H_
+
+void timer0_init(void);
+void delay_ms(uint16_t);
+
+
+#endif /* DELAY_H_ */

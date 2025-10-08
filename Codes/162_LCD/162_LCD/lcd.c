4b5944ecde54fab74ed318e3c13fc1bcb1e1842b
Sreejith Rajan <sreejithkriisc@gmail.com>
2025-10-08 20:31:14 +0530

    added 162 lcd functions

---
+++ Codes/162_LCD/162_LCD/lcd.c
@@ -0,0 +1,6 @@
+/*
+ * lcd.c
+ *
+ * Created: 08-10-2025 20:16:32
+ *  Author: sreej
+ */ 
---
+++ Codes/162_LCD/162_LCD/main.c
@@ -0,0 +1,18 @@
+/*
+ * 162_LCD.c
+ *
+ * Created: 08-10-2025 20:14:26
+ * Author : sreej
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
